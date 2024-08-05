630c56dcc8ad8b027ba1cbda21b86ee71bb12c6f
gopikrishna7780 <91692713554gopi@gmail.com>
2024-08-05 03:20:07 -0700

    Add files via upload
    gcff

---
+++ src/models/user.model.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const UserSchema = mongoose.Schema({
+    tid:{
+        type:Number,
+        required:true
+    },
+    tname: {
+        type:String,
+        required:true
+    },
+    taddress:{
+        type:String,
+        required:true
+    },
+    tage:{
+        type:Number,
+        required:true
+    },
+    tphone:{
+        type:Number,
+        required:true
+    },
+    temail:{
+        type:String,
+        required:true
+    },
+    tdob:{
+        type:Date,
+        required:true
+    },
+    is_active:  { type: Boolean, default: false },
+    is_verified:  { type: Boolean, default: false },
+    is_deleted:  { type: Boolean, default: false }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Topper', UserSchema);
